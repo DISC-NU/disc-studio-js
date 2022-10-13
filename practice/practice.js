033a5dc73cc2e6b74c23b865f97fc4d5f7224974
Dylan920424 <dylan920424@gmail.com>
2022-10-13 13:35:45 -0500

    add references and hints for practice

--- practice/practice.js
+++ practice/practice.js
@@ -1,1 +1,11 @@
 // TODO!!
+// GENERAL JS REFERENCES
+// Queryselector: https://www.w3schools.com/jsref/met_document_queryselector.asp
+// GetElementById: https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+// Setting and Changing style with [element name].style.[attribute]: https://www.w3schools.com/js/js_htmldom_css.asp
+// AddEventLisenter: https://www.w3schools.com/jsref/met_element_addeventlistener.asp
+
+// sample format for functions in JS
+// function_name = () => {
+//     a = 100;
+// }
